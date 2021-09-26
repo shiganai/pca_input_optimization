6964137eb0ba19674f06717048ea56b31f255cf2
shiganai <shiganai.hiro@gmail.com>
2021-09-26 15:37:44 +0900

    Update load_result_fromDir.m
    modified loading

--- curve_twice_velocity/load_result_fromDir.m
+++ curve_twice_velocity/load_result_fromDir.m
@@ -1,17 +1,17 @@
 AddPath_MATLABDATA
 
-base_dir = 'C:\Users\11900k\OneDrive - The University of Tokyo\MATLAB_data\pca_input_optimization\box_velocity\';
+[base_dir, username] = find_UserData;
+base_dir = [base_dir, '\pca_input_optimization\curve_twice\'];
+
+% base_dir = 'C:\Users\11900k\OneDrive - The University of Tokyo\MATLAB_data\pca_input_optimization\box_velocity\';
 wildcard = '*.m';
 
 count = 0;
 while true
-    target_dir = input('type para name / e to exit : ', 's');
-    if isequal(target_dir, 'e')
-        break
-    end
+    target_dir = uigetdir(base_dir);
     
-    % list = dir([base_dir])
-    list = dir([base_dir, target_dir]);
+    list = dir([target_dir])
+%     list = dir([base_dir, target_dir]);
     % list = dir([base_dir, target_dir, wildcard])
     
     target_index_array = 3:size(list, 1)-1;
@@ -28,14 +28,19 @@ while true
     
     if count
         hold(target_ax, 'on')
-        histogram(performance_array)
+        histogram(performance_array, 'DisplayName', Para.Mode_String)
         hold(target_ax, 'off')
     else
         dockfig(300)
         target_ax = gca;
-        histogram(performance_array)
+        histogram(performance_array, 'DisplayName', Para.Mode_String)
     end
     
     count = count + 1;
-        
+    
+    target_dir = input('type nothing / e to exit : ', 's');
+    if isequal(target_dir, 'e')
+        break
+    end
 end
+legend(target_ax)
